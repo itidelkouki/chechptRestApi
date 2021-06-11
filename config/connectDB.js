0134001ff9de9bceef8f7cf45a94095b53437755
ASUS <aatidelkouki@gmail.com>
2021-06-11 16:05:47 +0100

    important

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,10 +1,10 @@
 const mongoose=require("mongoose")
-
+require('dotenv').config({ path: './config/.env' });
 
 const connectDB=async ()=>{
     try {
         
-        await mongoose.connect('mongodb://localhost:27017/contactt-list',{ useUnifiedTopology: true,useNewUrlParser: true, useCreateIndex: true })
+        await mongoose.connect(process.env.MONGO_URI,{ useUnifiedTopology: true,useNewUrlParser: true, useCreateIndex: true })
         console.log("mongoose connected")
     } catch (error) {
         console.log(error)
